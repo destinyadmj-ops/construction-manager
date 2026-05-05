a0aa443b9a49f4baf65dc2fdf657c7abfe670abe
destinyadmj-ops <destinyadmj@gmail.com>
2026-05-06 03:42:23 +0900

    Auto-save sync: 2026-05-06 03:42:22

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 import WeekHub from './week-hub';
 
-export default function Home() {
+function isMobileUserAgent(userAgent: string) {
+  return /android|iphone|ipad|ipod|mobile/i.test(userAgent);
+}
+
+export default async function Home() {
+  const headerList = await headers();
+  const userAgent = headerList.get('user-agent') ?? '';
+  if (isMobileUserAgent(userAgent)) {
+    redirect('/mobile/week-hub');
+  }
+
   return <WeekHub />;
 }
